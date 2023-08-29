35364f22fae2f12522834b48c1413dbf1cb47cbd
Ravindu Hasanka <rhatu2000@gmail.com>
2023-08-29 08:51:01 +0530

    finalize

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,24 @@
+import scala.io.StdIn.readLine
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    //    question01
+    var input = readLine("Enter the number of temperatures in list :").split(",").toList
+    println(input)
+    var test = input.map(inpt => (((inpt.toInt*9.0)/5.0)+32)).reduce(_ + _)
+
+    println(test/input.length)
+
+//    question02
+
+//    def calculateLetters(values: List[String]): Int = {
+//
+//      val WordLen = values.map(word => word.length)
+//      WordLen.reduce(_ + _)
+//
+//    }
+//
+//     val values = List("apple", "banana", "cherry", "date")
+//     println("Total count of letter occurrences: " + calculateLetters(values))
   }
 }
--- src/question01.scala
+++ src/question01.scala
@@ -1,6 +1,6 @@
 import scala.io.StdIn._
-object Q1 {
-  println();
+
+object question01 {
   var n = readLine("Enter the number of temperatures in list :").split(" ").toList
   println(n)
 }
